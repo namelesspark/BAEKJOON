0505f2244e7d6398864ee6ce3415246c663847bb
namelesspark <jade.lake8852@gmail.com>
2025-03-30 23:24:43 +0900

    [Bronze V] Title: X보다 작은 수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N, X;
+
+	cin >> N >> X;
+
+	int* array = new int[N];
+
+	for (int i = 0; i < N; i++) {
+		cin >> array[i];
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (array[i] < X) {
+			cout << array[i] << " ";
+		}
+	}
+}
